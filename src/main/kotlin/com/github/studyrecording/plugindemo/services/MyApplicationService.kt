3e8dad23657b07faef57363f7a619b2e6f76c75c
GitHub Action <action@github.com>
2020-12-28 05:27:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/studyrecording/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/studyrecording/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.studyrecording.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.studyrecording.plugindemo.MyBundle
 
 class MyApplicationService {
 
